06a205b30988223c474e6a362c747ac0bc5243d0
yinshuxun <shuxun.yin@gmail.com>
2017-05-02 17:26:47 +0800

    buble add object.assign option add mockdata in focus

--- server/index.js
+++ server/index.js
@@ -3,6 +3,17 @@ import express from 'express'
 import {search} from '../utils/search'
 import {ana} from '../utils/dom-ana'
 
+const mData = ()=>{
+    return new Array(50).fill({
+        title:`测试数据${Math.floor(Math.random(100)*100)}`,
+        anchor: `主播${Math.floor(Math.random(100)*100)}`,
+        audienceNumber: Math.floor(Math.random(100)*100000),
+        snapshot: '',
+        url: '',
+        platformIcon: '',
+    })
+}
+
 let app = express();
 
 // 没有挂载路径的中间件，应用的每个请求都会执行该中间件
@@ -24,15 +35,22 @@ app.use(function (req, res, next) {
 
 // 挂载至 /user/:id 的中间件，任何指向 /user/:id 的请求都会执行它
 app.use('/liveapi/:id/:plat', function (req, res, next) {
+
     console.log('Request Type:', req.method);
     next();
 });
 
 // 路由和句柄函数(中间件系统)，处理指向 /user/:id 的 GET 请求
 app.get('/liveapi/:cate/:plat', function (req, res, next) {
-    search(req.params.cate, req.params.plat).then((ret) => {
-        res.send(ana(req.params.plat, ret))
-    })
+    const s = Date.now()
+    setTimeout(()=>{
+            res.send(mData())
+        console.log(`cost ${Date.now() - s} ms`)
+        },500)
+
+    // search(req.params.cate, req.params.plat).then((ret) => {
+    //     res.send(ana(req.params.plat, ret))
+    // })
 });
 
 
